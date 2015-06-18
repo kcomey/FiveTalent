01a704b2cdf8b6c5b4af8c8fddabc66a8290c0c6
Kendall Comey <kendall.comey@gmail.com>
2015-06-18 12:51:00 -0700

    Generate the MLS number

--- public/controllers/controllers.js
+++ public/controllers/controllers.js
@@ -3,6 +3,7 @@ var userController = angular.module('userController', []);
 userController.controller('userController', ['$scope', '$http',
   function ($scope, $http) {
     $scope.greet = "Kendall's Realtor App";
+    $scope.showMLSMessage = true;
 
     $http.get('/mls').success(function(response) {
       $scope.mlslist = response;
@@ -10,9 +11,15 @@ userController.controller('userController', ['$scope', '$http',
     });
 
   $scope.addListing = function() {
-    $http.post('/addmls', $scope.listing).success(function(response) {
-      $scope.mlslist.push(response);
-      $scope.listing = "";
+    $http.get('/getmls').success(function(mlsNum) {
+      $scope.listing = { mlsNum: mlsNum };
+
+      $http.post('/addmls', $scope.listing).success(function(response) {
+        $scope.mlslist.push(response);
+        $scope.listing = "";
+        $scope.showMLSMessage = true;
+        $scope.showMLS = false;
+      });
     });
   };
 
@@ -30,18 +37,19 @@ userController.controller('userController', ['$scope', '$http',
   $scope.edit = function(id) {
     $http.get('/editmls/' + id).success(function(response) {
       $scope.listing = response;
-      console.log(response);
+      $scope.showMLSMessage = false;
+      $scope.showMLS = true;
     });
   };
 
   $scope.update = function() {
-    $http.put('/updatemls/' + $scope.note._id, $scope.note)
+    $http.put('/updatemls/' + $scope.listing._id, $scope.listing)
       .success(function(response) {
-      for(var i = 0; i < $scope.notelist.length; i++) {
-        if($scope.notelist[i]._id === response._id) {
-          $scope.notelist[i].note = response.note;
-          $scope.notelist[i].author = response.author;
-          $scope.note = "";
+      for(var i = 0; i < $scope.mlslist.length; i++) {
+        if($scope.mlslist[i]._id === response._id) {
+          $scope.mlslist[i].mlsNum = response.mlsNum;
+          $scope.mlslist[i].salesPrice = response.salesPrice;
+          $scope.listing = "";
           break;
         }
       }
--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 'use strict';
 
+// require the npm modules I will be using
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+// require mongo, setup the db and collections, using mongojs module
 var mongo = require('mongo');
 var mongojs = require('mongojs');
-var db = mongojs('redb', ['mls', 'users']);
+var db = mongojs('redb', ['mls', 'users', 'mlsNumber']);
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
+// set up the express routes
+// get all of the listings
 app.get('/mls', function(req, res) {
   db.mls.find(function(err, docs) {
     res.json(docs);
   })
 });
 
+// get the record for a specific ID to populate fields to update listing
 app.get('/editmls/:id', function(req, res) {
   var id = req.params.id;
   db.mls.findOne({_id: mongojs.ObjectId(id)},function(err, docs) {
@@ -23,6 +28,56 @@ app.get('/editmls/:id', function(req, res) {
   })
 });
 
+// add a new listing
+app.post('/addmls', function(req, res) {
+  db.mls.insert(req.body, function(err, docs) {
+    res.json(docs);
+  });
+});
+
+// update a listing
+app.put('/updatemls/:id', function(req, res) {
+  var id = req.params.id;
+  delete req.body._id;
+
+  db.mls.findAndModify({
+    query: {_id: mongojs.ObjectId(id)},
+    update: req.body,
+    new: true
+    }, function(err, docs) {
+      console.log(docs);
+      res.json(docs);
+  });
+});
+
+// delete a listing
+app.delete('/deletemls/:id', function(req, res) {
+  var id = req.params.id;
+  db.mls.remove({_id: mongojs.ObjectId(id)}, function(err, docs) {
+    res.json(docs);
+  });
+});
+
+// get the MLS number to use
+app.get('/getmls', function(req, res) {
+  db.mlsNumber.find(function(err, docs) {
+    // create the collection if it does not exist
+    if (docs.length < 1) {
+      var mlsNumber = '10234';
+      db.mlsNumber.save( {mlsNum: mlsNumber});
+      res.json(mlsNumber);
+    }
+    else {
+    // otherwise, grab the number and increase by one
+      docs[0].mlsNum++;
+      db.mlsNumber.save( {mlsNum: docs[0].mlsNum, _id: docs[0]._id } );
+      res.json(docs[0].mlsNum);
+    }
+  })
+});
+
+// search on all possible fields, if user did not enter any input in a field
+// skip it. Would refactor this if I had more time probably.
 app.post('/search', function(req, res) {
   var searchString = [];
 
@@ -57,37 +112,7 @@ app.post('/search', function(req, res) {
   })
 });
 
-app.post('/addmls', function(req, res) {
-  res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
-  db.mls.insert(req.body, function(err, docs) {
-    res.json(docs);
-  });
-});
-
-app.put('/updatemls/:id', function(req, res) {
-  var id = req.params.id;
-  db.mls.findAndModify({query: {_id: mongojs.ObjectId(id)},
-    update: {$set:{note: req.body.note, author: req.body.author}},
-    new: true}, function(err, docs) {
-      res.json(docs)
-    });
-});
-
-app.delete('/deletemls/:id', function(req, res) {
-  var id = req.params.id;
-  db.mls.remove({_id: mongojs.ObjectId(id)}, function(err, docs) {
-    res.json(docs);
-  });
-});
-
+// start the server and listen for requests
 app.listen(process.env.PORT || 3000, function() {
   console.log('server started');
 });
-
-
-   // { bedrooms: req.body.bedrooms },
-    //{ bathrooms: req.body.bathrooms },
-    //{ squareFeet: req.body.squareFeet}
-    //{ dateListed: req.body.dateListed },
-    //{ city: req.body.city },
-    //{ state: req.body.state },
